918b613400b82b585ce79c14352a826d57aa3da8
rishit-singh <rsrishitsingh@gmail.com>
2024-05-29 14:47:42 -0700

    Merged changes.

--- src/app/Footer/footer.tsx
+++ src/app/Footer/footer.tsx
@@ -14,19 +14,3 @@ export default function Footer(){
         </footer>
     );
 }
-import Module from "module";
-import SponsorsLogosFooter from "./logos";
-
-export default function Footer(){
-    return(
-        <footer>
-            <div data-carousel="slide" className="logosSection w-[100%] flex flex-row justify-between overflow-x-hidden overflow-y-visible whitespace-nowrap p-[0.5em] slide-right">
-                <SponsorsLogosFooter></SponsorsLogosFooter>
-            </div>
-            <div className="flex flex-row items-center justify-center bg-neutral-700">
-                <p className="text-base m-[0.3em]">qwer*qwer*qwer*qwer</p>
-            </div>
-            
-        </footer>
-    );
-}
