4eab29a0d022dedc81fb75dc43debfe42ed4fe16
Anderson T <tseng.andersonn@gmail.com>
2024-06-05 12:31:05 -0700

    temporary cleanup of footer

--- src/app/Footer/footer.tsx
+++ src/app/Footer/footer.tsx
@@ -2,13 +2,14 @@ import SponsorsLogosFooter from "./logos";
 
 export default function Footer(){
     return(
-        <footer className="w-screen">
-            <div data-carousel="slide" className="logosSection w-[100%] flex flex-row justify-between overflow-x-hidden overflow-y-visible whitespace-nowrap p-[0.5em] slide-right">
+        <footer className="w-100">
+            <div data-carousel="slide" className="logosSection w-100 flex flex-row justify-between overflow-x-hidden overflow-y-visible whitespace-nowrap slide-right">
                 <SponsorsLogosFooter></SponsorsLogosFooter>
             </div>
-            <div className="flex flex-row items-center justify-center bg-neutral-700">
+
+            {/* <div className="flex flex-row items-center justify-center bg-neutral-700">
                 <p className="text-base m-[0.3em]">qwer*qwer*qwer*qwer</p>
-            </div>
+            </div> */}
             
         </footer>
     );
