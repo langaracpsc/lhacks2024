b99ebc0b98e644380ed8c7f82d657bbf877da372
rishit-singh <rsrishitsingh@gmail.com>
2024-06-25 04:27:58 -0700

    Fixed issues with headr

--- src/app/Header/header.tsx
+++ src/app/Header/header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import svggroup from "./Group_38.svg"
 
-export default function Header(){
+export default function Header() {
     const [displayCheck, setDisplayCheck] = useState(true);
     const [isOpen, setIsOpen] = useState(false);
     const toggleNavBar = () => {
@@ -21,117 +21,73 @@ export default function Header(){
             if (window.innerWidth < 1000) {
                 setDisplayCheck(false);
             }
-            else{
+            else {
                 setDisplayCheck(true);
             }
         };
 
-        handleResize(); 
+        handleResize();
 
         window.addEventListener('resize', handleResize);
 
         return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    function redirectAboutUs(){
+    function redirectAboutUs() {
         router.push("/aboutus")
     }
-    function redirectMain(){
+    function redirectMain() {
         router.push("/")
     }
-    function redirectHandbook(){
+    function redirectHandbook() {
         router.push("/Handbook")
-        
+
     }
 
 
-    return (
-        <header className={`flex flex-col items-center w-full h-[50vw] ${displayCheck ? 'bg-[url("/headerBackground2.jpg")] bg-center bg-no-repeat bg-cover' : ''}`}>
-            <div className="w-full h-full flex flex-col justify-center box-border bg-gradient-to-b from-transparent to-black to-75%">
-                <div className={`z-30 upperHeader flex w-full flex-row ${displayCheck ? 'p-[2em] justify-between': 'p-[2em]'} bg-black bg-opacity-90 fixed top-0 left-0 ${isOpen ? 'flex-col h-full justify-center': ''}`}>
-                    <div className={`buttons ml-[2em] ${displayCheck || isOpen ? '': 'hidden'} ${isOpen ? 'flex flex-col ml-0 ml-[10vw] w-[100%]': ''}`}>
-                        <button onClick={redirectMain} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>Main</button>
-                        <button onClick={redirectAboutUs} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>About Us</button>
-                        <button onClick={redirectHandbook} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>Handbook</button>
-                    </div>
     // picture not full screen as it was
-    return(
-        <header className={`flex flex-col items-center w-full h-[50vw] ${displayCheck ? 'bg-[url("/headerBackground2.jpg")] bg-center bg-no-repeat bg-cover' : ''}`}>
-            <div className="w-full h-full flex flex-col justify-center box-border bg-gradient-to-b from-transparent to-black to-75%">
-                <div className={`upperHeader flex w-full flex-row ${displayCheck ? 'p-[2em] justify-between': 'p-[2em]'} bg-black bg-opacity-90 fixed top-0 left-0 ${isOpen ? 'flex-col h-full justify-center': ''}`}>
-                    <div className={`buttons ml-[2em] ${displayCheck || isOpen ? '': 'hidden'} ${isOpen ? 'flex flex-col ml-0 ml-[10vw] w-[100%]': ''}`}>
-                        <button className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>Main</button>
-                        <button className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>About Us</button>
-                        <button className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl': 'text-lg'}`}>Handbook</button>
-                    </div>
-
-                    <div className={`links mr-[4em] ${displayCheck || isOpen ? '': 'hidden'} ${isOpen ? 'flex flex-col mr-0 ml-0 ml-[10vw] w-[100%]': ''}`}>
-                        <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl': 'text-lg'}`}>Sponsors</a>
-                        <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl': 'text-lg'}`}>FAQ</a>
-                    </div>
-                    <div className={`absolute right-[0.7em] top-[0.7em] ${displayCheck ? 'hidden' : ''}`}>
-                        <Hamburger direction="left" onToggle={toggleNavBar}></Hamburger>
-                    </div>
-                </div>
-                <div className={`central part flex flex-col w-full  ${displayCheck ? 'mt-[14em]' : 'mt-[8em]'} items-center`}>
-                    <h3 className={`${displayCheck ? 'text-xl' : 'text-base'}`}>September 28 - 29 Langara College</h3>
-                    <h1 className={`${displayCheck ? 'text-7xl' : 'text-4xl'}`}><span className='text-orange-600'>Langara</span> Hacks 2024</h1>
-                    <div className="buttons flex flex-row">
-                        <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-orange-600 py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Apply Now</button>
-                        <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-white py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Become a Mentor</button>
-                    </div>
-                </div>
-                    <div className={`links mr-[4em] ${displayCheck || isOpen ? '': 'hidden'} ${isOpen ? 'flex flex-col mr-0 ml-0 ml-[10vw] w-[100%]': ''}`}>
-                        <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl': 'text-lg'}`}>Sponsors</a>
-                        <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl': 'text-lg'}`}>FAQ</a>
-                    </div>
-                    <div className={`absolute right-[0.7em] top-[0.7em] ${displayCheck ? 'hidden' : ''}`}>
-                        <Hamburger direction="left" onToggle={toggleNavBar}></Hamburger>
-                    </div>
-                    
-                </div>
-                <div className={`central part flex flex-col w-full  ${displayCheck ? 'mt-[14em]' : 'mt-[8em]'} items-center`}>
-                    <h3 className={`${displayCheck ? 'text-xl' : 'text-base'}`}>September 21 - 22 Langara College</h3>
-                    <h1 className={`${displayCheck ? 'text-7xl' : 'text-4xl'}`}><span className='text-orange-600'>Langara</span> Hacks 2024</h1>
-                    <div className="buttons flex flex-row">
-                        <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-orange-600 py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Apply Now</button>
-                        <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-white py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Become a Mentor</button>
-                    </div>
+    return (<header className={`flex flex-col items-center w-full h-[50vw] ${displayCheck ? 'bg-[url("/headerBackground2.jpg")] bg-center bg-no-repeat bg-cover' : ''}`}>
+        <div className="w-full h-full flex flex-col justify-center box-border bg-gradient-to-b from-transparent to-black to-75%">
+            <div className={`z-30 upperHeader flex w-full flex-row ${displayCheck ? 'p-[2em] justify-between' : 'p-[2em]'} bg-black bg-opacity-90 fixed top-0 left-0 ${isOpen ? 'flex-col h-full justify-center' : ''}`}>
+                <div className={`buttons ml-[2em] ${displayCheck || isOpen ? '' : 'hidden'} ${isOpen ? 'flex flex-col ml-0 ml-[10vw] w-[100%]' : ''}`}>
+                    <button onClick={redirectMain} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl' : 'text-lg'}`}>Main</button>
+                    <button onClick={redirectAboutUs} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl' : 'text-lg'}`}>About Us</button>
+                    <button onClick={redirectHandbook} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl' : 'text-lg'}`}>Handbook</button>
                 </div>
 
-                <div className={`arrow flex flex-row w-[80%] justify-between self-center mt-[3em] ${displayCheck ? '': 'hidden'}`}>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Registration Opens</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>September 1</p>
-                    </div>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Registration Closed</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>September 15</p>
-                    </div>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Day of the event</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>September 28-29</p>
-                    </div>
+                <div className={`links mr-[4em] ${displayCheck || isOpen ? '' : 'hidden'} ${isOpen ? 'flex flex-col mr-0 ml-0 ml-[10vw] w-[100%]' : ''}`}>
+                    <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl' : 'text-lg'}`}>Sponsors</a>
+                    <a className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl' : 'text-lg'}`}>FAQ</a>
                 </div>
-                <img className='flex w-[80%] self-center' src='/Group_38.svg' alt="cool arrow below the schedule that represents the passage of time"/>
-            </div>
-                <div className={`arrow flex flex-row w-[80%] justify-between self-center mt-[3em] ${displayCheck ? '': 'hidden'}`}>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Registration Opens</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>Date</p>
-                    </div>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Registration Closed</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>Date</p>
-                    </div>
-                    <div className='flex flex-col justify-left mx-[5em]'>
-                        <h4 className='my-[0.2em] text-xl'>Day of the event</h4>
-                        <p className='text-l my-[0.2em] text-orange-600 text-l'>Date</p>
-                    </div>
+                <div className={`absolute right-[0.7em] top-[0.7em] ${displayCheck ? 'hidden' : ''}`}>
+                    <Hamburger direction="left" onToggle={toggleNavBar}></Hamburger>
                 </div>
-                <img className='flex w-[80%] self-center' src='/Group_38.svg'/>
-                
             </div>
-        </header>
+            <div className={`central part flex flex-col w-full  ${displayCheck ? 'mt-[14em]' : 'mt-[8em]'} items-center`}>
+                <h3 className={`${displayCheck ? 'text-xl' : 'text-base'}`}>September 28 - 29 Langara College</h3>
+                <h1 className={`${displayCheck ? 'text-7xl' : 'text-4xl'}`}><span className='text-orange-600'>Langara</span> Hacks 2024</h1>
+                <div className="buttons flex flex-row">
+                    <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-orange-600 py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Apply Now</button>
+                    <button className={`${displayCheck ? 'text-xl px-[2em]' : 'text-base px-[1em]'} m-[1em] border-2 border-white py-[0.3em] bg-black bg-opacity-70 rounded-md hover:scale-[1.05] transition-transform duration-300`}>Become a Mentor</button>
+                </div>
+            </div>
+
+            <div className={`arrow flex flex-row w-[80%] justify-between self-center mt-[3em] ${displayCheck ? '' : 'hidden'}`}>
+                <div className='flex flex-col justify-left mx-[5em]'>
+                    <h4 className='my-[0.2em] text-xl'>Registration Opens</h4>
+                    <p className='text-l my-[0.2em] text-orange-600 text-l'>September 1</p>
+                </div>
+                <div className='flex flex-col justify-left mx-[5em]'>
+                    <h4 className='my-[0.2em] text-xl'>Registration Closed</h4>
+                    <p className='text-l my-[0.2em] text-orange-600 text-l'>September 15</p>
+                </div>
+                <div className='flex flex-col justify-left mx-[5em]'>
+                    <h4 className='my-[0.2em] text-xl'>Day of the event</h4>
+                    <p className='text-l my-[0.2em] text-orange-600 text-l'>September 28-29</p>
+                </div>
+            </div>
+            <img className='flex w-[80%] self-center' src='/Group_38.svg' alt="cool arrow below the schedule that represents the passage of time" />
+        </div>
+    </header>
     );
 }
