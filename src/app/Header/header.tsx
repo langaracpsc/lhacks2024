6f78a8c4b090c9486f644ec240a98e94b7305c63
Anderson T <tseng.andersonn@gmail.com>
2024-07-12 17:53:56 -0700

    improvements to gradius and background opacity with header on mobile

--- src/app/Header/header.tsx
+++ src/app/Header/header.tsx
@@ -52,7 +52,7 @@ export default function Header() {
     
 
     return (
-        <div className={`z-30 upperHeader flex w-full flex-row p-[2em] fixed top-0 left-0 bg-black bg-opacity-30 ${displayCheck ? 'justify-between bg-gradient-to-b from-black from:10% to-transparent' : 'bg-gradient-to-b from-black to-transparent'}  ${isOpen ? 'flex-col h-full justify-center' : ''}`}>
+        <div className={`z-30 upperHeader flex w-full flex-row p-[2em] fixed top-0 left-0 ${displayCheck ? 'bg-black bg-opacity-30 justify-between bg-gradient-to-b from-black from:90% to-transparent' : 'bg-gradient-to-b from-black from:90% to-black/0'}  ${isOpen ? 'bg-black bg-opacity-50 flex-col h-full justify-center' : ''}`}>
             <div className={`buttons ml-[2em] ${displayCheck || isOpen ? '' : 'hidden'} ${isOpen ? 'flex flex-col ml-0 w-[100%]' : ''}`}>
                 <button onClick={redirectMain} className={`mr-[1em] hover:text-orange-600 font-bold text-left ${isOpen ? 'font-normal mr-0 my-[0.5em] border-b-[1px] border-solid border-white text-4xl' : 'text-lg'}`}>Home</button>
             </div>
@@ -60,7 +60,7 @@ export default function Header() {
                 <button onClick={() => { specialAnimationHandle('sponsors'); }} className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl' : 'text-lg'}`}>Sponsors</button>
                 <button onClick={() => { specialAnimationHandle('faq'); }} className={`ml-[1em] text-orange-600 hover:text-white text-left ${isOpen ? 'ml-0 my-[0.5em] border-b-[1px] border-solid border-orange-600 text-2xl' : 'text-lg'}`}>FAQ</button>
             </div>
-            <div className={`absolute right-[0.7em] top-[0.7em] ${displayCheck ? 'hidden' : ''}`}>
+            <div className={`absolute right-[0.7em] top-[0.7em] rounded-md bg-black bg-opacity-30 ${displayCheck ? 'hidden' : ''}`}>
                 <Hamburger direction="left" toggled={isOpen} onToggle={toggleNavBar}></Hamburger>
             </div>
         </div>
